3a04d259fe0f749697f0a2d4e661d01482450ff0
Ishmael <79452939+shihlabane@users.noreply.github.com>
2022-06-17 21:21:40 +0530

    Create ProgramThatAcceptTwoString.java

---
+++ ProgramThatAcceptTwoString.java
@@ -0,0 +1,17 @@
+package LanguageBasic;
+
+public class ProgramThatAcceptTwoString {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		/*
+		  
+		 */
+		
+		System.out.print(args[0] + " Technologies " + args[1]);
+		
+
+	}
+
+}
